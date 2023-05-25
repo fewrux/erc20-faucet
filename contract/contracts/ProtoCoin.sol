810a701cc1c2c179bde527a39c4d92bae3b1f4a2
Felipe Rubio <felipe.carvalho.rubio@gmail.com>
2023-05-25 10:38:45 -0300

    refactor: update mint to receive 'to' param and validate mintDelay on it

--- contract/contracts/ProtoCoin.sol
+++ contract/contracts/ProtoCoin.sol
@@ -5,28 +5,24 @@ pragma solidity ^0.8.19;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ProtoCoin is ERC20 {
-    address private _owner;
+    address private immutable _owner;
     uint256 private _mintAmount = 0;
     uint64 private _mintDelay = 60 * 60 * 24; // 1 day in seconds
 
-    mapping(address => uint256) private nextMind;
+    mapping(address => uint256) private nextMint;
 
     constructor() ERC20("ProtoCoin", "PRC") {
         _owner = msg.sender;
         _mint(msg.sender, 10000000 * 10 ** 18); // 10M tokens
     }
 
-    function mint() public {
+    function mint(address to) public restricted {
         require(_mintAmount > 0, "Minting is not enabled");
-        require(
-            block.timestamp > nextMind[msg.sender],
-            "Minting is not available yet"
-        );
+        require(block.timestamp > nextMint[to], "Minting is not available yet");
 
-        _mint(msg.sender, _mintAmount);
+        _mint(to, _mintAmount);
 
-        if (msg.sender != _owner)
-            nextMind[msg.sender] = block.timestamp + _mintDelay;
+        nextMint[to] = block.timestamp + _mintDelay;
     }
 
     function setMintAmount(uint256 newAmount) public restricted {
